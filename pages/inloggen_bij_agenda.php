9cc478a8c3d9538b469bdcaa00dd6a2acf25b7c9
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-11-13 21:14:19 +0100

    updat inloggen agenda

--- pages/inloggen_bij_agenda.php
+++ pages/inloggen_bij_agenda.php
@@ -7,9 +7,26 @@
 ?>
 <div id="email_agenda">
 
-<br>
+<br />
     <form>
-    E-mailadres <input type="text" name="emailadres">
-<br>
-    Wachtwoord <input type="text" name="password">
+        <table>
+            <tr>
+                <td>E-mailadres</td>
+                <td><input type="text" name="emailadres" /></td>
+            </tr>
+            <tr>
+                <td>Wachtwoord</td> 
+                <td><input type="text" name="password" /></td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit" name="submit" value="Inloggen" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <a Href="/wachtwoord_vergeten.php"> Wachtwoord vergeten</a>
+                </td>
+            </tr>
+        </table>
 </div>
