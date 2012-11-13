dca19e9dacd293f0307f4c51681a1b45c0e7f2c5
Sander Hiemstra <sander.hiemstra@gmail.com>
2012-11-13 20:46:26 +0100

    Updated bestellenproducten

--- pages/bestellenproducten.php
+++ pages/bestellenproducten.php
@@ -1,25 +1,25 @@
-
-        <form action="bestellenproducten">
-            <div><!--naam product--></div>
-            <div><!--afbeelding product--></div>
-            <div>Aantal:&nbsp<select name="aantalproducten">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    <option>6</option>
-                    <option>7</option>
-                    <option>8</option>
-                    <option>9</option>
-                    <option>10</option>
-                </select>
-                <!--Aantal producten-->
-            </div>
-            <div>Prijs:</div>
-            <div><!--prijs product--></div>
-            <div><input type="submit" value="bestel" name="Bestel" /></div>
-        </form>
-        <?php
-        // put your code here
-        ?>
+<form action="index.php?page=bestellenproducten">
+    <div><!--naam product--></div>
+    <div><!--afbeelding product--></div>
+    <div>Aantal:&nbsp&nbsp&nbsp&nbsp
+        <select name="aantalproducten">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+        </select>
+<!--Aantal producten-->
+    </div><br>
+    <div>Prijs:</div><br>
+    <div><!--prijs product--></div>
+    <div><input type="submit" value="Bestel" name="bestel" /></div>
+</form>
+<?php
+// put your code here
+?>
