cf282d96dde26679b96d1cafc48f0c3b7bf008f1
Jelle Smeets <jsmeets91@gmail.com>
2012-11-13 11:41:16 +0100

    Voorbeelden code toegevoegd.
    Voorbeelden met if statement en foreach voorbeeld.

---
+++ voorbeelden/voorbeelden.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Document by Jelle
+ * Created on 13-11-2012
+ * Version 1.0
+ */
+
+//standaard notatie if statement
+
+if($var == true){
+    //uitvoerbare code oa. echo var is true.
+}elseif($var2 == true){
+    //uitvoerbare code oa. echo var 2 is true.
+}else{
+    //uitvoerbare code echo else scenario.
+}
+   
+//foreach
+
+foreach($array as $key => $val){
+    //uitvoerbare code oa. echo $val['id'];
+}
+
+
