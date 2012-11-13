c3fb053c09299d4be24067b9205560c30003eacb
Jelle Smeets <jsmeets91@gmail.com>
2012-11-13 19:29:49 +0100

    Foutafhandeling databaseconnectie

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -4,10 +4,20 @@
  * Created on 13-11-2012.
  * Version 1.0
  */
-
+//maak globale variabele met instellingen.
+global $data;
+$data['develop'] = true;
 //regelt database connectie. standaard connectie voor sql in samenwerking met xampp.
 // laad de database pedicure.
+try{
  $db = new PDO('mysql:host=localhost;dbname=pedicure', 'root', '');
-
+}catch(Exception $e){
+    //als develop mode ingesteld is laat foutmelding zien.
+    if($data['develop'] == true){
+        echo 'Data base connection failed: '.$e->getMessage();
+    }else{
+        echo 'Problemen met het verbinden met de database.';
+    }
+}
 require_once('functions.php');
 ?>
