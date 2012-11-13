782d8c840273d8cdcd2f3fb01b9a157f00659d28
Sander Hiemstra <sander.hiemstra@gmail.com>
2012-11-13 20:25:05 +0100

    Ik heb bestellingproducten toegevoegd

---
+++ pages/bestellenproducten.php
@@ -0,0 +1,25 @@
+
+        <form action="bestellenproducten">
+            <div><!--naam product--></div>
+            <div><!--afbeelding product--></div>
+            <div>Aantal:&nbsp<select name="aantalproducten">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+                <!--Aantal producten-->
+            </div>
+            <div>Prijs:</div>
+            <div><!--prijs product--></div>
+            <div><input type="submit" value="bestel" name="Bestel" /></div>
+        </form>
+        <?php
+        // put your code here
+        ?>
