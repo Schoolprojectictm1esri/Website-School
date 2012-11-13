bd3fcda3f59f1ed3448b0e46f3ce5dfb30f7c5db
Thom Geertman <thom_geertman@hotmail.com>
2012-11-13 20:59:28 +0100

    Update bekijkenproducten

---
+++ pages/bekijkenproducten.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" type="text/css" href="style/thomstyle.css" />
+
+<div>   <SELECT NAME="Categorieen" MULTIPLE SIZE="16">
+            <OPTION VALUE="optie1">Optie 1</OPTION>
+            <OPTION VALUE="optie2">Optie 2</OPTION>
+            <OPTION VALUE="optie3">Optie 3</OPTION>
+            <OPTION VALUE="optie4">Optie 4</OPTION>
+            <OPTION VALUE="optie5">Optie 5</OPTION>
+            <OPTION VALUE="optie6">Optie 6</OPTION>
+            <OPTION VALUE="optie7">Optie 7</OPTION>
+            <OPTION VALUE="optie8">Optie 8</OPTION>
+            <OPTION VALUE="optie9">Optie 9</OPTION>
+            <OPTION VALUE="optie10">Optie 10</OPTION>
+            <OPTION VALUE="optie11">Optie 11</OPTION>
+            <OPTION VALUE="optie12">Optie 12</OPTION>
+            <OPTION VALUE="optie13">Optie 13</OPTION>
+            <OPTION VALUE="optie14">Optie 14</OPTION>
+            <OPTION VALUE="optie15">Optie 15</OPTION>
+            <OPTION VALUE="optie16">Optie 16</OPTION>
+        </SELECT>
+</div>
+<!--Categorien toevoegen van de producten--!>
+
+
+
+
+
+
+
+
+
+    
+    
+<?php
+// Thom
+?>
