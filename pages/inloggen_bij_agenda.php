3387d8849466e083abd13afa5c078e43c31c7633
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-11-14 01:22:30 +0100

    update inloggen bij agenda
    JELLE IK HEB JE HULP NODIG <3

--- pages/inloggen_bij_agenda.php
+++ pages/inloggen_bij_agenda.php
@@ -5,10 +5,23 @@
  * Version 1.0
  */
 ?>
+<?php
+session_start();
+$_SESSION["emailadres"] = $email;
+$_SESSION["password"] = $password;
+
+//afkortingen//
+$email = $_GET["emailadres"];
+$password = $_GET["password"];
+?>
 <div id="email_agenda">
 
-<br />
-    <form>
+<!--
+Zelfafhandelend formulier, controle op emailset, passoword set. Ja? verder naar controle met DB. Nee? Terug naar formulier
+-->
+<p />
+
+    <form action="index.php?" method="get">
         <table>
             <tr>
                 <td>E-mailadres</td>
@@ -25,8 +38,23 @@
             </tr>
             <tr>
                 <td colspan="2">
-                    <a Href="/wachtwoord_vergeten.php"> Wachtwoord vergeten</a>
+                    <a Href="index.php?page=inloggen_bij_agenda"> Wachtwoord vergeten</a>
                 </td>
             </tr>
         </table>
+
+<?php
+if(isset($_SESSION['emailadres']) == TRUE );{
+// actie 1
+    if(isset($_SESSION['password']) == TRUE );{
+        //actie 2
+        else{
+            //actie 3
+        }
+    }
+}
+else{ ;
+}
+
+?>
 </div>
