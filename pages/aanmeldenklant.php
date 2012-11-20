2133537c488a13041e8ad7cd487a99e6525460b3
Sander Hiemstra <sander.hiemstra@gmail.com>
2012-11-20 12:39:08 +0100

    Aanmelden klant en sanderstyle aangemaakt/aangepast

---
+++ pages/aanmeldenklant.php
@@ -0,0 +1,52 @@
+<form action="index.php?page=aanmeldenklant" method="POST">
+    <div id="test1"><table border="1">
+        <table>
+                <tr>
+                    <td>Email-adres:</td>
+                    <td><input type="text" name="email" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Voornaam:</td>
+                    <td><input type="text" name="voornaam" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Achternaam:</td>
+                    <td><input type="text" name="achternaam" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Woonplaats:</td>
+                    <td><input type="text" name="woonplaats" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Adres:</td>
+                    <td><input type="text" name="adres" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Postcode:</td>
+                    <td><input type="text" name="postcode" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Telefoonnummer:</td>
+                    <td><input type="text" name="telefoonnummer" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Geboortedatum:</td>
+                    <td><input type="text" name="geboortedatum" value="" /></td>
+                </tr>
+                <tr>
+                    <td>gegeven 9</td>
+                    <td><input type="text" name="gegeven9" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Wachtwoord</td>
+                    <td><input type="text" name="wachtwoord" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Herhaal wachtwoord</td>
+                    <td><input type="text" name="herhaalwachtwoord" value="" /></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Registreer" name="registreren" /></td>
+                </tr>
+        </table>
+</form>
