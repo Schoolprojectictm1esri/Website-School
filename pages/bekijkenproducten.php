2a1e3e0a079dd152d1ebe6253d3d48a0226db4cb
Thom Geertman <thom_geertman@hotmail.com>
2012-11-20 15:44:03 +0100

    Bekijkproduct , style

--- pages/bekijkenproducten.php
+++ pages/bekijkenproducten.php
@@ -1,26 +1,40 @@
 <link rel="stylesheet" type="text/css" href="style/thomstyle.css" />
 
-<div id="categories">   <SELECT NAME="Categorieen" MULTIPLE SIZE="5">
+<!-- Linkerblok !-->
+<div id="categories">  
+        <SELECT NAME="Categorieen" MULTIPLE SIZE="5">
             <OPTION VALUE="optie1">Optie 1</OPTION>
             <OPTION VALUE="optie2">Optie 2</OPTION>
             <OPTION VALUE="optie3">Optie 3</OPTION>
             <OPTION VALUE="optie4">Optie 4</OPTION>
             <OPTION VALUE="optie5">Optie 5</OPTION>
         </SELECT>
+</div> 
+
+
+<!-- Middenblok !-->
+<div id="resultaten">
 </div>
 
 
-<!--Categorien toevoegen van de producten--!>
+
+
+
+
+<!-- Rechterblok !-->
+<div id="productinfo"> 
+
+  <div id="bestelknop"> 
+      <form name='formulier' action='index.php?page=bestellenproducten' method='post'>
+      <input type="submit" name="submit" value="Bestel" />
+  </div>
+</div>
 
 
 
 
 
-
-
-
-
-    
+<div class="spacer"></div>
     
 <?php
 // Thom
