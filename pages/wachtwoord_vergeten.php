c27e1d42e7a00f04122c0c10e6a963715e703082
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-11-22 17:00:44 +0100

    html/css geschreven +beetje php
    code geschreven, strax nog auto-mail toevoegen en commentaar

--- pages/wachtwoord_vergeten.php
+++ pages/wachtwoord_vergeten.php
@@ -1,14 +1,91 @@
+<?php
+if(isset($_POST['submit'])){
+    if($_POST['emailadres'] != ''){
+        $stmt = $db->query("SELECT * FROM klanten WHERE `e-mail` = '".$_POST['emailadres']."'");
+        $result = $stmt->fetchObject();
+            if(!empty($result)){
+            echo 'Email word verzonden.';
+            }
+            else {
+?>
+            <div id="wwvergeten">
+                <form action="index.php?page=wachtwoord_vergeten" method="POST">
+                    <table>
+                            <!tekst !>
+                            <h2>Wachtwoord opvragen/wijzigen</h2>
+                            <p />
+                            Als je je wachtwoord bent vergeten of je wilt je wachtwoord wijzigen,   <br /> 
+                            voer dan hier je gebruikersnaam en het e-mailadres in waarmee je je     <br />
+                            aangemeld hebt. Je krijgt dan een e-mail waarmee je een nieuw wachtwoord kunt kiezen.
+                        <tr>
+                            <td>E-mailadres:</td>
+                            <td><input type="text" name="emailadres" /><td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" value="submit" name="submit"</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Onjuist emailadres</td>
+                        </tr>
+                    </table>
+                </form>  
+            </div>
+<?php
+            }
+    }
+    else {
+?>
+        <div id="wwvergeten">
+            <form action="index.php?page=wachtwoord_vergeten" method="POST">
+                <table>
+                        <!tekst !>
+                        <h2>Wachtwoord opvragen/wijzigen</h2>
+                        <p />
+                        Als je je wachtwoord bent vergeten of je wilt je wachtwoord wijzigen,   <br /> 
+                        voer dan hier je gebruikersnaam en het e-mailadres in waarmee je je     <br />
+                        aangemeld hebt. Je krijgt dan een e-mail waarmee je een nieuw wachtwoord kunt kiezen.
+                    <tr>
+                        <td>E-mailadres:</td>
+                        <td><input type="text" name="emailadres" /><td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" value="submit" name="submit"</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Vul emailadres in</td>
+                    </tr>
+                </table>
+            </form>  
+        </div>
+<?php
+        }
+}
+else {
+?>
 <div id="wwvergeten">
-    <form>
+    <form action="index.php?page=wachtwoord_vergeten" method="POST">
         <table>
+                <!tekst !>
+                <h2>Wachtwoord opvragen/wijzigen</h2>
+                <p />
+                Als je je wachtwoord bent vergeten of je wilt je wachtwoord wijzigen,   <br /> 
+                voer dan hier je gebruikersnaam en het e-mailadres in waarmee je je     <br />
+                aangemeld hebt. Je krijgt dan een e-mail waarmee je een nieuw wachtwoord kunt kiezen.
             <tr>
-                <td>E-mailadres</td>
+                <td>E-mailadres:</td>
                 <td><input type="text" name="emailadres" /><td>
             </tr>
             <tr>
                 <td></td>
-            </tr>
+                <td><input type="submit" value="submit" name="submit"</td>
+            </tr> 
         </table>
-    </form>
-    
+    </form>  
 </div>
+<?php 
+}
+?>
