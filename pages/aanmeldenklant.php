ea5da14f8d0af22892bb6a3c530fdade3cfab289
Sander Hiemstra <sander.hiemstra@gmail.com>
2012-11-22 13:37:35 +0100

    Update Aanmeldenklant + Style
    melding als er iets niet is ingevult gefixt
    waardes blijfen staan als hij het formulier opnieuw laat

--- pages/aanmeldenklant.php
+++ pages/aanmeldenklant.php
@@ -1,52 +1,70 @@
+<?php
+if(isset($_POST['registreren']))
+{
+    if($_POST['email']!='' || $_POST['voornaam']!='' || $_POST['achternaam']!=''
+        || $_POST['woonplaats']!='' || $_POST['adres']!=''
+        || $_POST['postcode']!='' || $_POST['telefoonnummer']!=''
+        || $_POST['wachtwoord']!=''|| $_POST['herhaalwachtwoord']!='')
+    {
+        echo 'test';
+    }
+    else{
+       print 'U bent vergeten een veld in te vullen';
+    }
+}else
+    {
+    }
+    
+$_POST['email'] = '';
+$_POST['voornaam'] = '';
+$_POST['achternaam'] = '';
+$_POST['woonplaats'] = '';
+$_POST['adres'] = '';
+$_POST['postcode'] = '';
+$_POST['telefoonnummer'] = '';
+$_POST['wachtwoord'] = '';
+$_POST['herhaaldwachtwoord'] = '';  
+?>
 <form action="index.php?page=aanmeldenklant" method="POST">
-    <div id="test1"><table border="1">
-        <table>
-                <tr>
+        <table border="0">
+            <tr>
                     <td>Email-adres:</td>
-                    <td><input type="text" name="email" value="" /></td>
+                    <td><input type="text" name="email" value="<?php echo $_POST['email']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Voornaam:</td>
-                    <td><input type="text" name="voornaam" value="" /></td>
+                    <td><input type="text" name="voornaam" value="<?php echo $_POST['voornaam']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Achternaam:</td>
-                    <td><input type="text" name="achternaam" value="" /></td>
+                    <td><input type="text" name="achternaam" value="<?php echo $_POST['achternaam']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Woonplaats:</td>
-                    <td><input type="text" name="woonplaats" value="" /></td>
+                    <td><input type="text" name="woonplaats" value="<?php echo $_POST['woonplaats']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Adres:</td>
-                    <td><input type="text" name="adres" value="" /></td>
+                    <td><input type="text" name="adres" value="<?php echo $_POST['adres']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Postcode:</td>
-                    <td><input type="text" name="postcode" value="" /></td>
+                    <td><input type="text" name="postcode" value="<?php echo $_POST['postcode']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Telefoonnummer:</td>
-                    <td><input type="text" name="telefoonnummer" value="" /></td>
-                </tr>
-                <tr>
-                    <td>Geboortedatum:</td>
-                    <td><input type="text" name="geboortedatum" value="" /></td>
-                </tr>
-                <tr>
-                    <td>gegeven 9</td>
-                    <td><input type="text" name="gegeven9" value="" /></td>
+                    <td><input type="text" name="telefoonnummer" value="<?php echo $_POST['telefoonnummer']; ?>" /></td>
                 </tr>
                 <tr>
                     <td>Wachtwoord</td>
-                    <td><input type="text" name="wachtwoord" value="" /></td>
+                    <td><input type="password" name="wachtwoord" value="" /></td>
                 </tr>
                 <tr>
                     <td>Herhaal wachtwoord</td>
-                    <td><input type="text" name="herhaalwachtwoord" value="" /></td>
+                    <td><input type="password" name="herhaalwachtwoord" value="" /></td>
                 </tr>
                 <tr>
-                    <td><input type="submit" value="Registreer" name="registreren" /></td>
+                    <td colspan="2"><input type="submit" value="Registreer" name="registreren" /></td>
                 </tr>
         </table>
 </form>
