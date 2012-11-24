b7b0299e4270bbef8e3c6c9acf2e296ac1145415
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-11-24 23:43:19 +0100

    update wachtwoord wijzigen

--- pages/wachtwoord_wijzigen.php
+++ pages/wachtwoord_wijzigen.php
@@ -10,7 +10,42 @@
 if(isset($_POST['submit'])){
     //controleert of alles is ingevult
     if(($_POST['ww1'] != '') and ($_POST['ww2'] != '')){
-        echo 'ok';
+        //controleer of wachtwoorden aan elkaar gelijk zijn
+        if(($_POST['ww1']) == ($_POST['ww2'])){
+            $ww1 = mysql_real_escape_string($_POST['ww1']);
+            $emailadres = $_POST['emailadres'];
+            $stmt = $db->query("update `klanten` SET `wachtwoord` = '".$ww1."', `emailadres` = '".$emailadres."'");
+            echo 'Uw wachtwoord is gewijzigd';
+        }
+        else {
+?> 
+        <!-- formulier !-->
+        <div id="wachtwoordwijzigen">
+            <form action="index.php?page=wachtwoord_wijzigen" method="POST">
+                <table>
+                    <tr>
+                        <td>Emailadres</td>
+                        <td>"'".$_POST['emailadres']."'"</td>
+                    </tr>
+                    <tr>
+                        <td>Wachtwoord</td>
+                        <td><input type="text" name="ww1"></td>
+                    </tr>
+                    <tr>
+                        <td>Herhaal<br />
+                            wachtwoord</td>
+                        <td><input type="text" name="ww2"</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input type="submit" name="submit" value="aanpassen" </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+<div class="error">Wachtwoorden niet gelijk aan elkaar.</div>  
+<?php
+        }
     }
     else{
  ?>
@@ -38,7 +73,7 @@ if(isset($_POST['submit'])){
             </table>
         </form>
     </div>
-<div class="error">Vul alstublieft alle velden in.</div>
+<div class="error">Vul alstublieft beide velden in.</div>
 
 <?php
     }
