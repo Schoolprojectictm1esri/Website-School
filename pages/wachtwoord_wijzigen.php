267bcff8c8fb6e688f12ebb4d59c108fcbbb5ff8
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-11-24 18:25:44 +0100

    update wachtwoord_wijzigen

--- pages/wachtwoord_wijzigen.php
+++ pages/wachtwoord_wijzigen.php
@@ -6,11 +6,15 @@
  */
 ?>
 <?php
+//kijkt of submit is
 if(isset($_POST['submit'])){
-    
-}
-else {
-?>
+    //controleert of alles is ingevult
+    if(($_POST['ww1'] != '') and ($_POST['ww2'] != '')){
+        echo 'ok';
+    }
+    else{
+ ?>
+    <!-- formulier !-->
     <div id="wachtwoordwijzigen">
         <form action="index.php?page=wachtwoord_wijzigen" method="POST">
             <table>
@@ -20,21 +24,51 @@ else {
                 </tr>
                 <tr>
                     <td>Wachtwoord</td>
-                    <td><input type="text" value="wachtwoordwijzigen1"></td>
+                    <td><input type="text" name="ww1"></td>
                 </tr>
                 <tr>
                     <td>Herhaal<br />
                         wachtwoord</td>
-                    <td><input type="text" value="wachtwordwijzigen2"</td>
+                    <td><input type="text" name="ww2"</td>
                 </tr>
                 <tr>
                     <td></td>
-                    <td><input type="submit" value="aanpassen" </td>
+                    <td><input type="submit" name="submit" value="aanpassen" </td>
                 </tr>
             </table>
         </form>
     </div>
+<div class="error">Vul alstublieft alle velden in.</div>
+
+<?php
+    }
+}
+else {
+?>
+<!-- formulier !-->
+<div id="wachtwoordwijzigen">
+    <form action="index.php?page=wachtwoord_wijzigen" method="POST">
+        <table>
+            <tr>
+                <td>Emailadres</td>
+                <td>".$_POST['emailadres']."</td>
+            </tr>
+            <tr>
+                <td>Wachtwoord</td>
+                <td><input type="text" name="ww1"></td>
+            </tr>
+            <tr>
+                <td>Herhaal<br />
+                    wachtwoord</td>
+                <td><input type="text" name="ww2"</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="submit" value="aanpassen" </td>
+            </tr>
+        </table>
+    </form>
+</div>
 <?php
 }
-
 ?>
