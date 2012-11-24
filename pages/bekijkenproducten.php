7d2bb66cfa3bbf46a2af301e86ee506fd87718a1
Thom Geertman <thom_geertman@hotmail.com>
2012-11-24 13:27:52 +0100

    Bekijkproduct, style

--- pages/bekijkenproducten.php
+++ pages/bekijkenproducten.php
@@ -1,38 +1,38 @@
-<link rel="stylesheet" type="text/css" href="style/thomstyle.css" />
 
 <!-- Linkerblok !-->
-<div id="categories">  
-        <SELECT NAME="Categorieen" MULTIPLE SIZE="5">
-            <OPTION VALUE="optie1">Optie 1</OPTION>
-            <OPTION VALUE="optie2">Optie 2</OPTION>
-            <OPTION VALUE="optie3">Optie 3</OPTION>
-            <OPTION VALUE="optie4">Optie 4</OPTION>
-            <OPTION VALUE="optie5">Optie 5</OPTION>
-        </SELECT>
-</div> 
+
+<?php
+
+
+$sql = "SELECT * FROM categorieen";
+$stmt = $db->query($sql);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+foreach ($result as $row){
+    var_dump($row);
+    print ('<a href="index.php?page=bekijkenproducten&categorie='.$row['id'].'">'.$row['naam'].'</a>');
+}
+    
+  
+?>
 
 
 <!-- Middenblok !-->
 <div id="resultaten">
+    hoi
 </div>
 
 
 
-
-
-
 <!-- Rechterblok !-->
+
 <div id="productinfo"> 
+
 
   <div id="bestelknop"> 
       <form name='formulier' action='index.php?page=bestellenproducten' method='post'>
       <input type="submit" name="submit" value="Bestel" />
   </div>
 </div>
-
-
-
-
 
 <div class="spacer"></div>
     
