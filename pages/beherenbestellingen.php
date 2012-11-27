7b8c5f3792f3403df1cb460c33f649fa8057f806
Thom Geertman <thom_geertman@hotmail.com>
2012-11-27 12:46:16 +0100

    update , beheren, bekijken, style

--- pages/beherenbestellingen.php
+++ pages/beherenbestellingen.php
@@ -9,25 +9,46 @@ and open the template in the editor.
         <title>Beheren bestellingen</title>
     </head>
     <body>
-        <?php
+ <?php
+ 
+ 
 $stmt = $db->query('SELECT * FROM bestelling1 b1 JOIN bestelling_producten1 bp ON b1.ID = bp.bestelling_id');
 $result = $stmt->fetchall();
 foreach ($result as $row){
     echo "<table width='600' cellpadding='5' cellspacing='5'>";
     echo "<tr><td colspan='2' class='vet'>Informatie </td></tr>";
-    echo "<tr><td>Bestelling ID</td><td>${row['bestelling_id']}</td></tr>";
-    echo "<tr><td>Klanten ID</td><td>${row['Klant_id']}</td></tr>";
-    echo "<tr><td>Datum geplaats</td><td>${row['datum']}</td></tr>";
-    echo "<tr><td>Datum afgerond</td><td>${row['afgerond']}</td></tr>";
-    echo "<tr><td>Product ID</td><td>${row['product_id']}</td></tr>";
-    echo "<tr><td>Aantal producten</td><td>${row['aantal']}</td></tr>";
-    echo "<tr><td><form name='formulier' action='index.php?page=verwijderproducten.php' method='post'>
-      <input type='submit' name='submit' value='Wijzigen' />";
+    echo "<tr><td>Bestelling ID</td><td>{$row['bestelling_id']}</td></tr>";
+    echo "<tr><td>Klanten ID</td><td>{$row['Klant_id']}</td></tr>";
+    echo "<tr><td>Datum geplaats</td><td>{$row['datum']}</td></tr>";
+    echo "<tr><td>Datum afgerond</td><td>{$row['afgerond']}</td></tr>";
+    echo "<tr><td>Product ID</td><td>{$row['product_id']}</td></tr>";
+    echo "<tr><td>Aantal producten</td><td>{$row['aantal']}</td></tr>";
+    echo "<tr><td><form name='formulier' action='index.php?page=beherenbestellingen' method='POST'>
+      <input type='submit' name='submit' value='Verwijder' />";
     echo "</table>";    
-}      
+     
+if($row['afgerond'] == '0000-00-00') {
+    print ('LET OP! Deze bestelling is nog niet afgerond!');
+}
+
+
+
+}
         ?>
     
-    
+          <?php
+         
+          
+
+          
+          if(isset($_POST['submit'])){
+             
+         
+         
+          
+      }
+
+      ?>
     
     
     </body>
--- pages/bekijkenproducten.php
+++ pages/bekijkenproducten.php
@@ -1,23 +1,16 @@
-
 <!-- Linkerblok !-->
 <div id="categories">
 Kies een categorie:
 <br>
 <br>
     
-    <?php
-
-
-
+<?php
 $stmt = $db->query('SELECT * FROM categorieen');
 $result = $stmt->fetchall();
 foreach ($result as $row){
 echo '<a href="index.php?page=bekijkenproducten&categorie='.$row['id'].'">'.$row['naam'].'</a>' ; echo "<br> <br>
     " ;
-
 } 
-
-
 ?>
 
 </div>
@@ -29,18 +22,12 @@ echo '<a href="index.php?page=bekijkenproducten&categorie='.$row['id'].'">'.$row
     <br>
    <?php
 $stmt2 = $db->query("SELECT * 
-FROM producten1
-WHERE categorieID = productID");
-$result2 = $stmt2-> fetchall();
-foreach ($result2 as $row2){
-echo '<a href="index.php?page=bekijkenproducten&categorie='.$row['id'].'&product='.$row2['productID'].'">'.$row2['naam'].'</a>' ; echo "<br> <br>";
-
+FROM producten1 where categorieID = " . $_GET['categorie']);
+$result2 = $stmt2->fetchall();
+    foreach ($result2 as $row2){
+        echo '<a href="index.php?page=bekijkenproducten&categorie='.$row['id'].'&product='.$row2['productID'].'">'.$row2['naam'].'</a>' ; echo "<br> <br>";
 }
-
-
-
-    
-   ?>
+?>
 </div>
 
 
@@ -48,11 +35,23 @@ echo '<a href="index.php?page=bekijkenproducten&categorie='.$row['id'].'&product
 <!-- Rechterblok !-->
 
 <div id="productinfo"> 
+    Productinformatie 
+    <br>
+    <br>
+<?php
+$stmt3 = $db->query("SELECT * FROM producten1  
+WHERE productID = " . $_GET['product']);
+$result3 = $stmt3->fetchall();
+    foreach ($result3 as $row3){
+        echo "Productnaam : {$row3['naam']}"; echo '<br>';
+        echo "Prijs : {$row3['prijs']}"; echo '<br>';
+ }
+?>
 
-
-  <div id="bestelknop"> 
-      <form name='formulier' action='index.php?page=bestellenproducten' method='post'>
+<div id="bestelknop"> 
+      <form name='formulier' action='index.php?page=bestellenproducten' method='GET'>
       <input type="submit" name="submit" value="Bestel" />
+      </form>
   </div>
 </div>
 
