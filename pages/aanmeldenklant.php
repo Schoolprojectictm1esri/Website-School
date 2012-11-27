057eea960401de9409c5dfad64bb5b4f347d7cce
Sander Hiemstra <sander.hiemstra@gmail.com>
2012-11-27 12:11:28 +0100

    update aanmelden klant

--- pages/aanmeldenklant.php
+++ pages/aanmeldenklant.php
@@ -1,32 +1,6 @@
 <?php
 if(isset($_POST['registreren']))
-{
-    if($_POST['email']!='' || $_POST['voornaam']!='' || $_POST['achternaam']!=''
-        || $_POST['woonplaats']!='' || $_POST['adres']!=''
-        || $_POST['postcode']!='' || $_POST['telefoonnummer']!=''
-        || $_POST['wachtwoord']!=''|| $_POST['herhaalwachtwoord']!='')
-    {
-        $email = $_POST['email'];
-        $voornaam = $_POST['voornaam'];
-        $achternaam = $_POST['achternaam'];
-        $woonplaats = $_POST['woonplaats'];
-        $adres = $_POST['adres'];
-        $postcode = $_POST['postcode'];
-        $telefoonnr = $_POST['telefoonnummer'];
-        $sql="insert into klanten (email, wachtwoord, /*registratiedatum*/, voornaam, achternaam, adres, woonplaats, postcode, telefoonnr) 
-                values ('$email', '$voornaam', '$achternaam', '$woonplaats', '$adres', '$postcode', '$telefoonnr');";
-        $sth = $db->prepare($sql);
-        $success = $sth->execute();
-                //versturen naar database
-    }
-    else{
-       print 'U bent vergeten een veld in te vullen';
-    }
-}else
-    {
-    }
-    
-?>
+{ ?>
 <form action="index.php?page=aanmeldenklant" method="POST">
         <table border="0">
             <tr>
@@ -70,3 +44,77 @@ if(isset($_POST['registreren']))
                 </tr>
         </table>
 </form>
+<?php
+    if($_POST['email']!='' || $_POST['voornaam']!='' || $_POST['achternaam']!=''
+        || $_POST['woonplaats']!='' || $_POST['adres']!=''
+        || $_POST['postcode']!='' || $_POST['telefoonnummer']!=''
+        || $_POST['wachtwoord']!=''|| $_POST['herhaalwachtwoord']!='')
+    {
+		if ($_POST['wachtwoord'] == $_POST['herhaalwachtwoord'])
+		{
+        $email = $_POST['email'];
+        $voornaam = $_POST['voornaam'];
+        $achternaam = $_POST['achternaam'];
+        $woonplaats = $_POST['woonplaats'];
+        $adres = $_POST['adres'];
+        $postcode = $_POST['postcode'];
+        $telefoonnr = $_POST['telefoonnummer'];
+        $sql="insert into klanten (email, wachtwoord, /*registratiedatum*/, voornaam, achternaam, adres, woonplaats, postcode, telefoonnr) 
+                values ('$email', '$voornaam', '$achternaam', '$woonplaats', '$adres', '$postcode', '$telefoonnr');";
+        $sth = $db->prepare($sql);
+        $success = $sth->execute();
+        }
+		else{
+			print 'Wachtwoorden komen niet overeen';
+			}
+    }
+    else{
+       print 'Een veld is nog niet ingevuld';
+        }
+}
+else{
+?>
+<form action="index.php?page=aanmeldenklant" method="POST">
+        <table border="0">
+            <tr>
+                    <td>Email-adres:</td>
+                    <td><input type="text" name="email" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Voornaam:</td>
+                    <td><input type="text" name="voornaam" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Achternaam:</td>
+                    <td><input type="text" name="achternaam" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Woonplaats:</td>
+                    <td><input type="text" name="woonplaats" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Adres:</td>
+                    <td><input type="text" name="adres" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Postcode:</td>
+                    <td><input type="text" name="postcode" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Telefoonnummer:</td>
+                    <td><input type="text" name="telefoonnummer" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Wachtwoord</td>
+                    <td><input type="password" name="wachtwoord" value="" /></td>
+                </tr>
+                <tr>
+                    <td>Herhaal wachtwoord</td>
+                    <td><input type="password" name="herhaalwachtwoord" value="" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Registreer" name="registreren" /></td>
+                </tr>
+        </table>
+</form>
+<?php } ?>
