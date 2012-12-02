529bc403eca362d8454d2797b75b70d265110490
Botje <botje88@gmail.com>
2012-12-02 23:11:22 +0100

    Aanpassen Klant/Product.

--- pages/Aanpassenklantgegevens.php
+++ pages/Aanpassenklantgegevens.php
@@ -1,35 +1,3 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
-<!DOCTYPE html>
-<html>
-            <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <link rel="stylesheet" type="text/css" href="danielstyle.css" />
-            <title>Aanpassen klantgegevens</title>
-            </head>
-    <body>
-    
-    
-
-<?php
-            if(isset($details['wijzigen']))
-            {
-                if($details['e-mail'] != "" 
-                || $details['achternaam'] != "" 
-                || $details['naam'] != "" 
-                || $details['woonplaats'] != "" 
-                || $details['telnr'] != "" 
-                || $details['postcode'] != "" );
-            
-                
-            else{
-                   echo 'Alles moet ingevuld zijn .$leegvakje. is niet ingevuld';
-                }
-            }
-
-?>
 <?php   
  
             
@@ -41,49 +9,148 @@ and open the template in the editor.
             //print_r($result);
         
         
-?>
-        
-    <form action="" method ="post">
+?>    
+
+<form action="" method ="post">
                 
-        <table border="1">
+            <table border="0">
             
-            <td colspan="3">Klant gegevens</td>
+            <td>Klant gegevens</td>
             <tr></tr>
             
+            <td>Klant nummer: </td>
+            <td> <input type="text" name ="klantnummer" value="<?php echo $details['id'];?>"</td>
+            <td></tr>
             <td>Email:</td>
-            <td><input type="text" name="wEmail" value="<?php echo $details['email']; ?>"<td>
+            <td><input type="text" name="email" value="<?php echo $details['email']; ?>"<td>
             <tr></tr>
             
             <td>Voornaam:</td>
-            <td><input type="text" name="wVoornaam" value="<?php echo $details['naam']; ?>"></td>
+            <td><input type="text" name="voornaam" value="<?php echo $details['naam']; ?>"></td>
             <tr></tr>
             
             <td>Achternaam:</td>
-            <td><input type="text" name="wAchternaam" value="<?php echo $details['achternaam']; ?>"></td>
+            <td><input type="text" name="achternaam" value="<?php echo $details['achternaam']; ?>"></td>
             <tr></tr>
             
             <td>Adres:</td>
-            <td><input type="text" name="wAdres" value="<?php echo $details['registratiedatum']; ?>"</td>
+            <td><input type="text" name="straat" value="<?php echo $details['straat']; ?>"</td>
+            <input type="text" name="huisnummer" value="<?php echo $details['huisnummer']; ?>"
             <tr></tr>
             
             <td>Woonplaats:</td>
-            <td><input type="text" name="wWoonplaats" value="<?php echo $details['woonplaats']; ?>"</td>
+            <td><input type="text" name="woonplaats" value="<?php echo $details['woonplaats']; ?>"</td>
             <tr></tr>
             
             <td>Postcode:</td>
-            <td><input type="text" name="wPostcode" value="<?php echo $details['postcode']; ?>"</td>
+            <td><input type="text" name="postcode" value="<?php echo $details['postcode']; ?>"</td>
             <tr></tr>
             
             <td>Telnr:</td> 
-            <td><input type="text" name="wTelnr" value="<?php echo $details['telnr']; ?>"</td>
+            <td><input type="text" name="telnr" value="<?php echo $details['telnr']; ?>"</td>
                        
             
             
-        </table>
-     <input type="submit" name="verwijderen" value="Verwijder geselecteerde gegevens">
-        <input type="submit" name="Wijzig" value="Wijzigingen opslaan">
-        <input type="submit" name="annuleren" value="Annuleren">
+            </table>
+        
+            <input type="submit" name="verwijderen" value="Verwijder deze klant">
+            <input type="submit" name="wijzig" value="Wijzigingen opslaan">
+            <input type="submit" name="annuleren" value="Annuleren">
     </form>
+<?php
+            if(isset($_POST['wijzig']))
+            {
+                
+
+?>
+
+        
+    <form action="index.php?page=aanpassenklantgegegevens" method ="post">
+                
+            <table border="0">
+            
+            <td>Klant gegevens</td>
+            <tr></tr>
+            
+            <td>Klant nummer: </td>
+            <td> <input type="text" name ="klantid" value="<?php echo $details['id'];?>"</td>
+            <td></tr>
+            <td>Email:</td>
+            <td><input type="text" name="email" value="<?php echo $details['email']; ?>"<td>
+            <tr></tr>
+            
+            <td>Voornaam:</td>
+            <td><input type="text" name="voornaam" value="<?php echo $details['naam']; ?>"></td>
+            <tr></tr>
+            
+            <td>Achternaam:</td>
+            <td><input type="text" name="achternaam" value="<?php echo $details['achternaam']; ?>"></td>
+            <tr></tr>
+            
+            <td>Adres:</td>
+            <td><input type="text" name="straat" value="<?php echo $details['straat']; ?>"</td>
+            <input type="text" name="huisnummer" value="<?php echo $details['huisnummer']; ?>"
+            <tr></tr>
+            
+            <td>Woonplaats:</td>
+            <td><input type="text" name="woonplaats" value="<?php echo $details['woonplaats']; ?>"</td>
+            <tr></tr>
+            
+            <td>Postcode:</td>
+            <td><input type="text" name="postcode" value="<?php echo $details['postcode']; ?>"</td>
+            <tr></tr>
+            
+            <td>Telnr:</td> 
+            <td><input type="text" name="telnr" value="<?php echo $details['telnr']; ?>"</td>
+                       
+            
+            
+            </table>
+        
+            <input type="submit" name="verwijderen" value="Verwijder deze klant">
+            <input type="submit" name="wijzig" value="Wijzigingen opslaan">
+            <input type="submit" name="annuleren" value="Annuleren">
+    </form>
+
+<?php
+                if($_POST['email'] != ""  
+                || $_POST['voornaam'] != "" 
+                || $_POST['achternaam'] != "" 
+                || $_POST['naam'] != "" 
+                || $_POST['woonplaats'] != "" 
+                || $_POST['telnr'] != "" 
+                || $_POST['postcode'] != ""
+                || $_POST['straat'] != ""
+                || $_POST['huisnummer'] != "");
+              
+              
+              
+            else{
+                   echo 'Alles moet correct ingevuld zijn';
+                }
+                $email      = $_POST['email'];
+                $voornaam   = $_POST['voornaam'];
+                $achternaam = $_POST['achternaam'];
+                $woonplaats = $_POST['woonplaats'];
+                $telnr      = $_POST['telnr'];
+                $postcode   = $_POST['postcode'];
+                $straat     = $_POST['straat'];
+                $huisnummer = $_POST['huisnummer'];
+                $sql=("UPDATE behandelingen 
+                SET email = '$email', voornaam = '$voornaam', achternaam = '$achternaam', woonplaats = '$woonplaats', telnr = '$telnr', straat = '$straat', huisnummer = '$huisnummer', where id = id");
+                
+              
+                
+                $sth = $db->prepare($sql);
+                $doorvoeren = $sth->execute();
+                echo 'Gegevens zijn gewijzigd.';
+             
+            
+            
+            ?>
+  
     
-    </body>
-</html>
+    <?php
+    }
+    ?>
+
