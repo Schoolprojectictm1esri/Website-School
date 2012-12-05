87a5b4ed4d618099a432d3b5317ead1ff2a37d48
Jelle Smeets <jsmeets91@gmail.com>
2012-12-05 13:08:35 +0100

    Routebeschrijving ANWB update

--- pages/routebeschrijving.php
+++ pages/routebeschrijving.php
@@ -1,1 +1,3 @@
-<script type="text/javascript" src="http://www.anwb.nl/verkeer/verkeerstool.js"></script>
+<div id="routeplanner">
+    <script type="text/javascript" src="http://www.anwb.nl/verkeer/verkeerstool.js"></script>
+</div>
