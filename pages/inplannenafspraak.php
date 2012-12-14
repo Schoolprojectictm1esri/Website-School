f7802a7e13cdb2c77e0bef8a60ce8593ef5f6b61
Thom Geertman <thom_geertman@hotmail.com>
2012-12-14 14:37:54 +0100

    inplan

--- pages/inplannenafspraak.php
+++ pages/inplannenafspraak.php
@@ -4,58 +4,60 @@
     <head>       
         <title>Inplannen afspraak</title>
     </head>
-    <body>
-        <?php
+    <body> 
+      <?php
         
         if(isset($_GET['date']))
             {
-              $date = $_GET['date'];
-              
+              $date = $_GET['date'];              
               print ("U heeft de volgende datum geselecteerd: $date <br>");          
-              print ("Op deze dag zijn de volgende tijden mogelijk: <br>");
+              print ("Op deze dag zijn de volgende tijden bezet: <br>");
               
+
              
-              $tijdenLijst=$db->query("SELECT * from AFSPRAKEN where datum LIKE '$date%'");
+                     
+                     $tijdenLijst=$db->query("SELECT * from afspraken where datum LIKE '$date%' ORDER BY datum ASC");
+             
               if($tijdenLijst != FALSE){
                
               $result = $tijdenLijst->fetchall();
               
-                                   
+                                     
               foreach ($result as $tijden)
-              {
-                   
-                
- 
+              { 
                   print ("<br>");
-                  print ("{$tijden['datum']} <br> ");
                   
-                                   
                   $time = date('H:i:s', strtotime($tijden['datum']));
                   print ($time);
-              }
-             
-              }
-             
-              
+                 
+                  
+                   
+              }            
+              }                         
               else{
                   print ("Er zijn op de door u gekozen dag geen afspraken");
-              }
+              }                            
+                     }
               
-              
-              }
-              else {
-                  print ("<br>");
+             else {
+                   print ("<br>");
                    print ("Let op! Er is geen datum geselecteed <br>");
               }
-              
-     
-        
-              
-        
+            
+             
+
           
         
+//SELECT *
+//FROM afspraken AS a
+//JOIN afspraakbehandelingen AS ab ON a.id = ab.afspraak_id
+//JOIN behandelingen as b ON ab.behandeling_id = b.id    
+     
        
+
+        
      
         ?>
+ 
     </body>   
 </html>
