433eebdc88519881fc8fad5b8396a0b4ce0f6904
Jelle Smeets <jsmeets91@gmail.com>
2012-12-18 11:52:31 +0100

    Pedicurepraktijk een woord gemaakt.

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
-
 /**
  * document by Jelle Smeets.
  * Created on 13-11-2012.
- * Last updated on: 15-11-2012 By Jelle
- * Version 1.0
  */
 ob_start();
 session_start();
@@ -32,15 +29,13 @@ require_once('includes/bootstrap.php');
                     var anwb_tool_nr="63";
                     //-->
                 </script>
-                
-
-        <?php
-    }
-}
-?>
+             <?php
+            }
+        }
+        ?>
         <!-- $title komt uit de bootstrap !-->
         <title> 
-            Pedicure praktijk D&#233;sir&#233;e <?php echo $title; ?>
+            Pedicurepraktijk D&#233;sir&#233;e <?php echo $title; ?>
         </title>
         <meta charset="UTF-8">
         <!-- Verschillende stylesheets om elkaar niet in de weg te zitten. Aan het einde van project samenvoegen !-->
@@ -57,7 +52,7 @@ require_once('includes/bootstrap.php');
             <!-- Div for header!-->
             <div id="header">
                 <div id="contact-info-header">
-                    Pedicure praktijk D&#233;sir&#233;e <br/>
+                    Pedicurepraktijk D&#233;sir&#233;e <br/>
                     T: 06-41544308 <br />
                     E: <a href="mailto:desiree.vermeulen@home.nl">desiree.vermeulen@home.nl</a> <br />
                     Leerinkbeek 63 <br />
@@ -96,48 +91,48 @@ require_once('includes/bootstrap.php');
                     </div>!-->
                 </div>
                 <div id="content-right">
-<?php
-//haal pagina op en laad de pagina.
-if (isset($_GET['page'])) {
-    //kijk of de pagina een string is.
-    if (is_string($_GET['page'])) {
-        //kijk of het bestand bestaat.
-        if (file_exists('pages/' . $_GET['page'] . '.php')) {
-            //laad de opgevraagde pagina die bestaat.
-            if (isset($pagelist[$_GET['page']]) && is_numeric($pagelist[$_GET['page']])) {
-                if (getRole() >= $pagelist[$_GET['page']]) {
-                    require_once('pages/' . $_GET['page'] . '.php');
-                } else {
-                    echo '<p class="permission_error">U heeft onvoldoende rechten om deze pagina te bekijken. <br />
-                                        Mocht deze fout zich vaker voordoen terwijl u wel voldoende rechten heeft, neemt u dan alstublieft contact op met de  Pedicure praktijk D&#233;sir&#233;e.
-                                        </p>';
-                }
-            } else {
-                //anonieme gebruikers zijn toegestaan.
-                require_once('pages/' . $_GET['page'] . '.php');
-            }
-        } else {
-            //geen pagina gevonden.
-            echo 'De opgegeven pagina bestaat niet.';
-        }
-    } else {
-        //get variabele voldoet niet aan de gestelde eisen.
-        echo 'U heeft geen geldige pagina opgegeven.';
-    }
-} else {
-    //homepagina wordt geladen.
-    require_once('pages/home.php');
-}
-?>
+                    <?php
+                    //haal pagina op en laad de pagina.
+                    if (isset($_GET['page'])) {
+                        //kijk of de pagina een string is.
+                        if (is_string($_GET['page'])) {
+                            //kijk of het bestand bestaat.
+                            if (file_exists('pages/' . $_GET['page'] . '.php')) {
+                                //laad de opgevraagde pagina die bestaat.
+                                if (isset($pagelist[$_GET['page']]) && is_numeric($pagelist[$_GET['page']])) {
+                                    if (getRole() >= $pagelist[$_GET['page']]) {
+                                        require_once('pages/' . $_GET['page'] . '.php');
+                                    } else {
+                                        echo '<p class="permission_error">U heeft onvoldoende rechten om deze pagina te bekijken. <br />
+                                                            Mocht deze fout zich vaker voordoen terwijl u wel voldoende rechten heeft, neemt u dan alstublieft contact op met de  Pedicure praktijk D&#233;sir&#233;e.
+                                                            </p>';
+                                    }
+                                } else {
+                                    //anonieme gebruikers zijn toegestaan.
+                                    require_once('pages/' . $_GET['page'] . '.php');
+                                }
+                            } else {
+                                //geen pagina gevonden.
+                                echo 'De opgegeven pagina bestaat niet.';
+                            }
+                        } else {
+                            //get variabele voldoet niet aan de gestelde eisen.
+                            echo 'U heeft geen geldige pagina opgegeven.';
+                        }
+                    } else {
+                        //homepagina wordt geladen.
+                        require_once('pages/home.php');
+                    }
+                    ?>
                 </div>
                 <div class="spacer"></div>
             </div>
             <div id="footer">
                     <?php
                     if (date('Y') == 2012) {
-                        echo 'Copyright &copy;  Pedicure praktijk D&#233;sir&#233;e 2012';
+                        echo 'Copyright &copy;  Pedicurepraktijk D&#233;sir&#233;e 2012';
                     } else {
-                        echo 'Copyright &copy;  Pedicure praktijk D&#233;sir&#233;e 2012 -  ' . date("Y");
+                        echo 'Copyright &copy;  Pedicurepraktijk D&#233;sir&#233;e 2012 -  ' . date("Y");
                     }
                     ?> 
             </div>
@@ -145,5 +140,5 @@ if (isset($_GET['page'])) {
     </body>
 </html>
 <?php
-ob_end_flush();
+    ob_end_flush();
 ?>
