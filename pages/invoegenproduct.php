d06310d06e72f4d2333f503e6c6b9ca9afb9ae9a
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-12-20 18:16:43 +0100

    update (en nu ga ik eten)

--- pages/invoegenproduct.php
+++ pages/invoegenproduct.php
@@ -1,7 +1,44 @@
 <?php
-
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+  Document   : invoegenproduct.php
+  Created on : 20-12-2012
+  Author     : Thomas
+  invoeren van producten.
  */
+?>
+<?php
+
+if(isset($_POST['submit'])){
+    $stmt = $db->prepare("INSERT INTO `producten`(`naam`, `prijs`, `omschrijving`, `actief`, `foto`, `categorieid`) 
+                                       VALUES    (naam = :naam,
+                                            prijs = :prijs, 
+                                            omschrijving = :omschrijving,
+                                            actief = :actief, 
+                                            categorieid = :categorieid)");
+
+            $stmt->bindParam(':naam', $_POST['naam']);
+            $stmt->bindParam(':prijs', $_POST['prijs']);
+            $stmt->bindParam(':omschrijving', $_POST['omschrijving']);
+            $stmt->bindParam(':actief', $_POST['actief']);
+            $stmt->bindParam(':categorieid', $_POST['categorieid']);
+            $stmt->execute();
+}
+else {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
