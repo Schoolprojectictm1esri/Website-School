ee77e029fdd6c289732fb40502a94159804aa308
Jelle Smeets <jsmeets91@gmail.com>
2012-12-20 14:50:47 +0100

    Verschillende kleine updates

--- pages/agenda.php
+++ pages/agenda.php
@@ -7,54 +7,41 @@
         Het weergeven van de agenda.
 */
     include './includes/Date.php';
-
     $date = new Date();
-
     echo "Plaatje met legenda";
-
-
     // Vandaag
     $date->now();
-
     echo "<table id=table_agenda>";
-
     // Voor elke row in de table
     for($row = 0; $row<4; $row++)
     {                
         echo "<tr>";
-
         // Voor elke column in de table
         for($column = 0; $column<7; $column++)
         {        
-
             $stmt = $db->prepare("SELECT COUNT(lengte) FROM behandelingen WHERE actief = 'TRUE'");
             $stmt->execute();
             $numberOfApp = $stmt->fetchall();
-
             // Kijkt welke dag het is, en kijk wat de beschikbaarheid op die dag is.
                                         // Afhankelijk van die beschikbaarheid geeft het een kleur weer.
             if ($date->dayOfWeek()=="Zondag"){
                 $available ="agendaholiday";
-
-            }
-                else {   
-
-                    if      ( $numberOfApp <= 8)
-                        $available = "agendaavail";
-
-                    else if ( $numberOfApp >= 9 && $numberOfApp <= 14)
-                        $available = "agendabusy";
-
-                    else if ( $numberOfApp >= 15)
-                        $available = "agendafull";
-
-                    else  
-                        $available = "agendavacancy";
+            }else{   
+                if( $numberOfApp <= 8){
+                    $available = "agendaavail";
+                }elseif( $numberOfApp >= 9 && $numberOfApp <= 14){
+                    $available = "agendabusy";
+                }elseif( $numberOfApp >= 15){
+                    $available = "agendafull";
+                }else{
+                    $available = "agendavacancy"; 
+                }  
+                        
                      }
                 echo "<td class='$available'>";
                 // Link om agenda te openen met link waarin dag maand jaar staan.
 
-                Echo "<a href='index.php?page=inplannenafspraak&datum=".$date->day."-".$date->month."-".$date->year."'>";
+                Echo "<a href='index.php?page=inplannenafspraak&date=".$date->day."-".$date->month."-".$date->year."'>";
 
 
 
