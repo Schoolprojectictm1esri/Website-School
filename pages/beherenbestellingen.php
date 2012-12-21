b36c209a7b94479e3009cca9ec479bb4812372f3
Thomas Vermeulen <thomasvermeulen_2@hotmail.com>
2012-12-21 15:30:44 +0100

    update pagina deel 1

--- pages/beherenbestellingen.php
+++ pages/beherenbestellingen.php
@@ -1,48 +1,63 @@
-<!-- Auteur : Thom Geertman -->
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Beheren bestellingen</title>
-    </head>
-    <body>
-        <div class="inzienklantgegevens">
- <?php
-
-
-    
-$stmt = $db->prepare('SELECT * FROM bestelling b1 JOIN bestelling_producten bp ON b1.ID = bp.bestelling_id');
+<?
+/*Auteur : Thom Geertman
+*  update by Thomas Vermeulen
+*   21-12-2012
+*/
+?>
+<?php
+$stmt = $db->prepare("SELECT `klant_id`, `id`, `datum` FROM `bestelling` WHERE `afgerond` IS NULL");
 $stmt->execute();
-
-
-
-// query om benodigde gegevens 
-
-    if($stmt != FALSE)
-        {
-    
 $result = $stmt->fetchall();
 
-foreach ($result as $row){
-    echo "<table>";
-    echo "<tr><td colspan='2' class='vet'>Informatie </td></tr>";
-    echo "<tr><td>Bestelling ID</td><td>{$row['bestelling_id']}</td></tr>";
-    echo "<tr><td>Klanten ID</td><td>{$row['klant_id']}</td></tr>";
-    echo "<tr><td>Datum geplaats</td><td>{$row['datum']}</td></tr>";
-    echo "<tr><td>Datum afgerond</td><td>{$row['afgerond']}</td></tr>";
-    echo "<tr><td>Product ID</td><td>{$row['product_id']}</td></tr>";
-    echo "<tr><td>Aantal producten</td><td>{$row['aantal']}</td></tr>";
-    echo "<a href=index.php?page=verwijderbestelling&id={$row['id']}>Verwijderen</A>";
-    // linkje naar het verwijderen van een bestelling uit de database
-    echo "</table>";    
-// alle resultaten worden weergegeven in een tabel over desbetreffende bestelling
-    
-                        }
-      }        
-        ?>
-  
-        </div>
-    
-    
-    </body>
-</html>
+if(!empty($result)){
+    if(isset($_GET['id'])){
+?>
+<form action="index.php?page=beherenbestellingen" method="POST">
+    <table>
+    <tr>
+        <th>Klant ID:</th>
+        <th>Product ID:</th>
+        <th>Besteldatum:</th>
+    </tr>
+<?php
+?>
+    </table>
+</form>
+<?php
+    }
+    else{
+?>
+        <form action="index.php?page=beherenbestellingen" method="POST">
+            <table>
+                <tr>
+                    <th>Bestelling ID:</th>
+                    <th>Klant ID:</th>
+                    <th>Besteldatum:</th>
+                </tr>
+<?php
+                foreach($result as $een => $rij){        
+?>
+                <tr>
+                    <td><?php echo $rij['id'] ?></td>
+                    <td><?php echo $rij['klant_id'] ?></td>
+                    <td><?php echo $rij['datum'] ?></td>
+                    <td><a href="index.php?page=beherenbestellingen&id=<?php echo $rij['id'] ?>">Status aanpassen</a></td>
+                </tr>
+<?php
+                }
+?>               
+            </table>
+        </form>
+<?php
+
+    }
+}
+else{
+    echo 'Er zijn geen onafgeronde bestellingen';
+    echo 'Ook afgeronde bestellingen zien?';
+    if(isset($_POST['submit2'])){
+        //maken van query en alles in foreach zetten
+    }
+}
+
+?>
