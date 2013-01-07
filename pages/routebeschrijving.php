f144cb08f104df0825ee45d291788394aea3aadd
Jelle Smeets <jsmeets91@gmail.com>
2013-01-07 13:09:29 +0100

    Kleine style updates

--- pages/routebeschrijving.php
+++ pages/routebeschrijving.php
@@ -3,9 +3,11 @@
  * 
  */
 ?>
+<div class="route_text">
+Vul hieronder uw postcode, huisnummer en woonplaats in en de route naar pedicurepraktijk D&#233;sir&#233;e wordt berekend.<br />
+!Let op er wordt een extra scherm geopend: ANWB Routeplanner!
+</div>
 <div id="routeplanner">
-    Vul hieronder uw postcode, huisnummer en woonplaats in en de route naar pedicurepraktijk D&#233;sir&#233;e wordt berekend.<br />
-    !Let op er wordt een extra scherm geopend: ANWB Routeplanner!
     <!-- Laad routeplanner vanaf de anwb dit wordt getoont in een iframe !-->
     <script type="text/javascript" src="http://www.anwb.nl/verkeer/verkeerstool.js"></script>
 </div>
